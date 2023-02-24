b2d54d0711c36a3f861e7fe4bcc30960b45b191c
Dinesh Rawat <dineshrawat@Dineshs-MacBook-Pro.local>
2023-02-24 14:54:42 +0530

    added paths

--- src/index.js
+++ src/index.js
@@ -4,19 +4,23 @@
  * @param {number} [totalPoints=20] - The number of points to generate along the path.
  * @returns {Array<{x: number, y: number}>} - An array of points along the SVG path.
  */
-export default function svgToPolygonPoints(pathString, totalPoints = 20) {
+export default svgToPolygonPoints(pathString, totalPoints = 20,      = "0 0 500 500", width = 500, height = 500) {
     if (!pathString) {
         return true;
     }
 
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('viewBox', viewBox);
+    svg.setAttribute('width', width);
+    svg.setAttribute('height', height);
+
     const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
     path.setAttribute('d', pathString);
     svg.appendChild(path);
 
     const canvas = document.createElement('canvas');
-    canvas.width = 500;
-    canvas.height = 500;
+    canvas.width = width;
+    canvas.height = height;
     const ctx = canvas.getContext('2d');
 
     const points = [];
